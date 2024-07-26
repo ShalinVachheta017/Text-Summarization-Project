110d19c0b8e86cb87cfa6a27f90f1e339db151ac
ShalinVachheta017 <shalinvachheta2016@hotmail.com>
2024-07-26 19:56:05 +0200

    training remaing and evaluation code done

---
+++ src/textSummarizer/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,14 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.model_evaluation import ModelEvaluation
+from textSummarizer.logging import logger
+
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_trainer_config()
+        model_evaluation_config = ModelEvaluation(config=model_evalution_config)
+        model_evaluation_config.evaluate()
